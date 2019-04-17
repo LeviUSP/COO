55a51fb8ee17cf29cc6fae5b2e2e44f86bbf9747
Levi <levifernandes@usp.br>
2019-04-17 01:09:53 -0300

    Getters and Setters para os atributos e verificação se o email é válido

--- Pessoa.java
+++ Pessoa.java
@@ -1,10 +1,32 @@
-public abstract class Pessoa
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+public class Pessoa
 {
 	private String nome;
 	private String email;
 
-	public abstract String getNome();
-	public abstract void setNome(String nome);
-	public abstract String getEmail();
-	public abstract void setEmail(String email);
+	public String getNome(){
+		return this.nome;
+	}
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	public String getEmail(){
+		return this.email;
+	}
+	public void setEmail(String email){
+		if(validarEmail(email))
+			this.email = email;
+		else
+			System.out.println("Esse email nao eh valido");
+	}
+
+	private boolean validarEmail(String email){
+		Pattern padraoDeEmail = Pattern.compile("^[(a-zA-Z-0-9-\\_\\+\\.)]+@[(a-z-A-z)]+\\.[(a-zA-z)]{2,3}$");
+		Matcher padraoSeguido = padraoDeEmail.matcher(email);
+
+		return padraoSeguido.matches();
+	} 
 }
