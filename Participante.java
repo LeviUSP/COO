7dd774570afa2b1a37fc6038ce831f57e2c71280
joseUSP <40243650+joseUSP@users.noreply.github.com>
2019-04-18 01:51:55 -0300

    implementação da classe Participante

--- Participante.java
+++ Participante.java
@@ -1,9 +1,22 @@
-public class Participante{
-    String nome;
-    String enderecoEletronico;
-    Data inicio;
-    Data fim;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-    public void indicaDisponibilidade(){
-    }
+
+public class Participante extends Pessoa
+{
+	private ArrayList<Horario> horarios;
+
+	Participante(){
+		this.horarios = new ArrayList<>();
+	}
+	public void adicionaHorario(LocalDateTime inicio, LocalDateTime fim){
+		Horario h = new Horario(inicio, fim);
+		horarios.add(h);					
+	}
+	public Horario getHorario(int indice){
+		if(indice < 0 || indice >= horarios.size())
+			return null;
+		else
+			return horarios.get(indice);
+	}
 }
