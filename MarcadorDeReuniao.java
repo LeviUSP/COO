8dab41e1682d82e1db5b9e174b27f8ff0d238737
Levi <levifernandes@usp.br>
2019-04-23 14:01:57 -0300

    Marcador de Reuniao alterado

--- MarcadorDeReuniao.java
+++ MarcadorDeReuniao.java
@@ -1,13 +1,30 @@
 import java.util.LinkedList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public abstract class MarcadorDeReuniao
+public class MarcadorDeReuniao
 {
 	private Reuniao reuniao;
 
-	public abstract void marcarReuniaoEntre(LocalDate dataInicial, LocalDate dataFinal, LinkedList<String> listaDeParticipantes);
-	public abstract void indicaDisponibilidadeDe(String participante, LocalDateTime inicio, LocalDateTime fim);
-	public abstract void mostraSobreposicao();
+	public void marcarReuniaoEntre(LocalDate dataInicial, LocalDate dataFinal, Collection<String> listaDeParticipantes){
+		HashMap<String, Participante> participantes = new HashMap<>();
+		
+		for (String interador : listaDeParticipantes){
+			Participante atual = new Participante();
+			atual.setNome(interador);
+			participantes.put(interador, atual);
+		}
+	
+	
+		reuniao = new Reuniao(dataInicial, dataFinal);
+
+	}
+	public void indicaDisponibilidadeDe(String participante, LocalDateTime inicio, LocalDateTime fim){
+
+	}
+	public void mostraSobreposicao(){
+
+	}
 }
--- Reuniao.java
+++ Reuniao.java
@@ -1,17 +1,34 @@
 import java.util.HashMap;
 import java.time.LocalDate;
 
-public abstract class Reuniao
+public class Reuniao
 {
 	private final Pessoa organizador;
 	private final HashMap<String, Participante> participantes;
 	private final LocalDate dataInicial;
 	private final LocalDate dataFinal;
 
-	public abstract Participante buscaParticipante(String participante);
-	public abstract Pessoa getOrganizador();
-	public abstract LocalDate getDataInicial();
-	public abstract LocalDate getDataFinal();
-	public abstract void imprimeDadosDosParticipantes();
-	public abstract void imprimeDadosDaReuniao();
+	Reuniao(LocalDate dataInicial, LocalDate dataFinal){
+		this.dataInicial = dataInicial;
+		this.dataFinal = dataFinal;
+	}
+	
+	public Participante buscaParticipante(String participante){
+
+	}
+	public Pessoa getOrganizador(){
+
+	}
+	public LocalDate getDataInicial(){
+
+	}
+	public LocalDate getDataFinal(){
+
+	}
+	public void imprimeDadosDosParticipantes(){
+
+	}
+	public void imprimeDadosDaReuniao(){
+
+	}
 }
