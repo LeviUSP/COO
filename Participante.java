20140a89a23ccfb55ca845688e6f72dd371a6bc4
joseUSP <40243650+joseUSP@users.noreply.github.com>
2019-04-28 18:35:04 -0300

    Update Participante.java

--- Participante.java
+++ Participante.java
@@ -6,7 +6,7 @@ public class Participante extends Pessoa
 {
 	private ArrayList<Intervalo> intervalos;
 
-	Participante(){
+	public Participante(){
 		this.intervalos = new ArrayList<>();
 	}
 	public void adicionaIntervalo(LocalDateTime inicio, LocalDateTime fim){
@@ -18,5 +18,28 @@ public class Participante extends Pessoa
 			return null;
 		else
 			return intervalos.get(indice);
+	}
+	public ArrayList<Intervalo> getIntervalos(){
+		return this.intervalos;
+	}
+	public void imprimeDadosDoParticipante(){
+            System.out.println("Nome: " + super.getNome());
+	    
+            if(intervalos.isEmpty()){
+                System.out.println("Nao ha horarios de disponibilidade declarados por esse usuario!");
+                return;
+            }
+            
+            int contador = 0;
+            
+            System.out.println("Horarios declarados:");
+            
+            for(Intervalo i: intervalos)
+            {
+                contador++;
+                System.out.println("Intervalo " + contador + ":");
+                System.out.println("De " + i.getInicio().toLocalDate().toString() + " - " + i.getInicio().toLocalTime().toString());
+                System.out.println("Ate " + i.getFim().toLocalDate().toString() + " - " + i.getFim().toLocalTime().toString());
+            }
 	}
 }
