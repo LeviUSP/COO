cb14d753c50ac2121059ec18b30d43992880b9d0
joseUSP <40243650+joseUSP@users.noreply.github.com>
2019-04-28 18:38:27 -0300

    implementação de imprime dados

--- Reuniao.java
+++ Reuniao.java
@@ -1,17 +1,37 @@
 import java.util.HashMap;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-public abstract class Reuniao
+public class Reuniao
 {
-	private final Pessoa organizador;
 	private final HashMap<String, Participante> participantes;
 	private final LocalDate dataInicial;
 	private final LocalDate dataFinal;
 
-	public abstract Participante buscaParticipante(String participante);
-	public abstract Pessoa getOrganizador();
-	public abstract LocalDate getDataInicial();
-	public abstract LocalDate getDataFinal();
-	public abstract void imprimeDadosDosParticipantes();
-	public abstract void imprimeDadosDaReuniao();
+	public Reuniao(LocalDate dataInicial, LocalDate dataFinal, HashMap<String,Participante> participantes){
+		this.dataInicial = dataInicial;
+		this.dataFinal = dataFinal;
+		this.participantes = participantes;
+	}	
+	public Participante buscaParticipante(String participante){
+		return participantes.get(participante);
+	}
+	public LocalDate getDataInicial(){
+		return this.dataInicial;
+	}
+	public LocalDate getDataFinal(){
+		return this.dataFinal;
+	}
+	public void imprimeDadosDaReuniao(){
+            System.out.println("Dados dos participantes:");
+		
+            ArrayList<Participante> listaParticipante = new ArrayList<>(participantes.values());
+		
+            for(Participante p: listaParticipante){
+                p.imprimeDadosDoParticipante();
+            }
+	}
+	public HashMap<String, Participante> getParticipantes(){
+		return this.participantes;
+	}
 }
